374ca8a6e35bf8824718f0ceee2e26ecf0a0fa11
Lili Boxer <ninaliboxer@gmail.com>
2019-06-24 10:59:14 -0700

    alternative more efficient way in comments

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function createDog(name, age, weight) {
 const dog = createDog('Boo', 8, 50);
 
 console.log(dog);
+
+// alternative efficient way
+
+// const createDog = (name, age, weight) => ({ name, age, weight });
+
+// const dog = createDog('Same', 2) 20
